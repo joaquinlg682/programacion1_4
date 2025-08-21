d4eff97d4892e4e24886be31733375e62b904bee
joaquinlg682 <joaquinlg682@gmail.com>
2025-08-21 20:58:18 -0300

    primer commit

---
+++ ejercicio complementarios secuenciales/ejercicios_complementarios_secuenciales.py
@@ -0,0 +1,81 @@
+print("EJERCICIO 1")
+numero1=10
+print(numero1)
+
+print("EJERCICIO 2")
+numero2=3.50
+print(numero2)
+
+print("EJERCICIO 3")
+suma=numero1+numero2
+print(suma)
+
+print("EJERCICIO 4")
+resta=numero1-numero2
+multiplicacion=numero1*numero2
+division=numero1/numero2
+
+print(resta)
+print(multiplicacion)
+print(division)
+
+print("EJERCICIO 5")
+nombre='joaquin'
+print(nombre)
+
+print("EJERCICIO 6")
+precio=50.20
+print(f"el precio del articulo es: {precio}")
+
+print("EJERCICIO 7")
+descuento=0.25
+print("el descuento es del 25%")
+
+print("EJERCICIO 8")
+precio_final=precio-(precio*descuento)
+print(f"precio final con descuento incluido: {precio_final}")
+
+print("EJERCICIO 9")
+cadena="hola me gustan las papas"
+print(f"{cadena}")
+
+print("EJERCICIO 10")
+longitud=len(cadena)
+print(f"la longitud de la cadena es: {longitud}")
+
+print("EJERCICIO 11")
+precio2=10.2
+entero=int(precio2)
+print(entero)
+
+print("EJERCICIO 12")
+apellido="Godoy"
+nombre_completo=nombre+ " " +apellido
+print(f"{nombre_completo}")
+
+print("EJERCICIO 13")
+edad=19
+print(f"{edad} años")
+edad=edad+5
+print(f"sumado 5 años: {edad}")
+edad=edad-10
+print(f"menos 10 seria: {edad}")
+
+print("EJERCICIO 14")
+altura=1.83
+print(f"mi altura es: {altura}")
+altura=altura*5
+print(f"mi altura miltiplicada por 5 es: {altura}")
+altura=altura-10
+print(f"mi altura ahora disminuida con 10 es: {altura}")
+
+print("EJERCICIO 15")
+nombre_mayus="JOAQUIN"
+print(nombre_mayus)
+nombre_minus=nombre_mayus.lower()
+print(nombre_minus)
+
+print("EJERCICIO 16")
+nombre2="jOAQUIN"
+nombre_pri=nombre2.capitalize()
+print(nombre_pri)
